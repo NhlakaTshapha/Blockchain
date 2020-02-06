77bc926795aeda23d007ad5ebe5b1e4d7dfec624
NhlakaTshapha <45420727+NhlakaTshapha@users.noreply.github.com>
2020-02-06 14:51:47 +0200

    initial commit

---
+++ BlockChain/Blockchain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public class Blockchain
+    {
+        //to add a difficulty
+        public int Difficulty { get; set; } = 3;
+        public IList<Block> Chain { set; get; }
+
+        public Blockchain()
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            //previous code before adding difficulty
+            //block.Hash = block.CalculateHash();
+            block.Mine(this.Difficulty);
+            Chain.Add(block);
+        }
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BlockChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var starttime = DateTime.Now;
+
+            Blockchain MenziCoin = new Blockchain();
+            MenziCoin.CreateTransaction(new Transaction("Henry", "MaHesh", 10));
+            MenziCoin.ProcessPendingTransactions("Bill");
+
+            MenziCoin.CreateTransaction(new Transaction("MaHesh", "Henry", 5));
+            MenziCoin.CreateTransaction(new Transaction("MaHesh", "Henry", 5));
+            MenziCoin.ProcessPendingTransactions("Bill");
+
+            var endTime = DateTime.Now;
+
+            Console.WriteLine($"Duration: {endTime - starttime}");
+
+            Console.WriteLine("=========================");
+            Console.WriteLine($"Henry' balance: {MenziCoin.GetBalance("Henry")}");
+            Console.WriteLine($"MaHesh' balance: {MenziCoin.GetBalance("MaHesh")}");
+            Console.WriteLine($"Bill' balance: {MenziCoin.GetBalance("Bill")}");
+
+            Console.WriteLine("=========================");
+            Console.WriteLine($"MenziCoin");
+            Console.WriteLine(JsonConvert.SerializeObject(MenziCoin, Formatting.Indented));
+
+            Console.ReadKey();
+        }
+    }
+}
